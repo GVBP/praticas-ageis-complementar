90d520232fa3400422a78937cf9e9604211cea17
Guilherme Pereira <38007365+GVBP@users.noreply.github.com>
2023-09-26 23:26:50 -0300

    Update atividade-03-b.py

--- atividade-03/atividade-03-b.py
+++ atividade-03/atividade-03-b.py
@@ -8,5 +8,9 @@ class TestContarCaracteres(unittest.TestCase):
         resultado = contarCaracteres("Esta é uma frase de teste")
         self.assertEqual(resultado, 27)
 
+    def test_frase_vazia(self):
+        resultado = contarCaracteres("")
+        self.assertEqual(resultado, 0)
+
 if __name__ == '__main__':
     unittest.main()
